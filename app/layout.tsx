d621a165d143647e7a1ba1b87ec014c0f570fc8e
kysclient <kysclient@gmail.com>
2025-05-16 14:58:18 +0900

    feat: 스파이시 익명 게시판

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,32 @@
-import type React from "react"
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import "./globals.css"
-import { Toaster } from "@/components/ui/toaster"
-import { getAdminStatus } from "@/lib/actions"
-import { Tabs } from "@/components/tabs"
+import type React from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { Toaster } from "@/components/ui/toaster";
+import { getAdminStatus } from "@/lib/actions";
+import { Tabs } from "@/components/tabs";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "익명 게시판",
   description: "익명으로 글을 작성하고 볼 수 있는 게시판입니다.",
-    generator: 'v0.dev'
-}
+  generator: "kim yu shin",
+};
 
 export default async function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
-  const isAdmin = await getAdminStatus()
+  const isAdmin = await getAdminStatus();
 
   return (
     <html lang="ko">
       <body className={inter.className}>
-        <div className="container max-w-4xl py-10">
+        <div className="container max-w-4xl py-10 mx-auto">
           <header className="mb-6">
-            <h1 className="text-3xl font-bold mb-4">익명 게시판</h1>
+            <h1 className="text-3xl font-bold mb-4">스파이시 익명우편함 💌</h1>
             <Tabs isAdmin={isAdmin} />
           </header>
           {children}
@@ -34,5 +34,5 @@ export default async function RootLayout({
         <Toaster />
       </body>
     </html>
-  )
+  );
 }
